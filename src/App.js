93917dd619d82562a609daba2be001a67620d024
mb2o <mboomaars@gmail.com>
2019-07-08 16:55:13 +0200

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Default from './pages/Default';
 import Home from './pages/Home';
 import Recipes from './pages/Recipes';
 import Single from './pages/Single';
-import Default from './pages/Default';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -10,12 +11,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home />
-        <Recipes />
-        <Single />
-        <Default />
-      </div>
+      <Router>
+        <main>
+          {/* Add Navbar here */}
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/recipes" exact component={Recipes} />
+            <Route path="/recipes/:id" exact component={Single} />
+            <Route component={Default} />
+          </Switch>
+        </main>
+      </Router>
     );
   }
 }
