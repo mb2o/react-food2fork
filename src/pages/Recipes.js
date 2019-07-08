1fd1b8d50bd9f272addc92022990d857c55e899f
mb2o <mboomaars@gmail.com>
2019-07-08 20:08:43 +0200

    Recipe Page Ajax Request

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,17 +1,38 @@
 import React, { Component, Fragment } from 'react';
 import RecipeList from '../components/RecipeList';
 import Search from '../components/Search';
-import { recipeData } from '../data/tempList';
 
 export default class Recipes extends Component {
   constructor(props) {
     super(props);
+
+    this.getRecipes = this.getRecipes.bind(this);
   }
 
   state = {
-    recipes: recipeData,
-    search: ''
+    recipes: [],
+    search: '',
+    url: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+    }`
   };
+
+  async getRecipes() {
+    try {
+      const response = await fetch(this.state.url);
+      const responseData = await response.json();
+
+      this.setState({
+        recipes: responseData.recipes
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getRecipes();
+  }
 
   handleChange = e => {
     this.setState({
