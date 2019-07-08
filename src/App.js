31ff9481678e4605d8ce960f7c5112e4aef5907e
mb2o <mboomaars@gmail.com>
2019-07-08 16:32:03 +0200

    Github - Netlify Continuous Deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <h1 className="text-slanted text-orange letter-spacing">App</h1>
       </div>
     );
   }
